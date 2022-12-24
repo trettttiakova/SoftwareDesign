e6371761f913c085b374b3b80e892b9664e21873
Zoe Tretiakova <trettttiakpva@gmail.com>
2022-12-24 21:15:59 +0300

    Add implementation and tests

---
+++ src/main/java/HW8/Clock.java
@@ -0,0 +1,7 @@
+package HW8;
+
+import java.time.Instant;
+
+public interface Clock {
+    Instant now();
+}
---
+++ src/main/java/HW8/NormalClock.java
@@ -0,0 +1,10 @@
+package HW8;
+
+import java.time.Instant;
+
+public class NormalClock implements Clock {
+    @Override
+    public Instant now() {
+        return Instant.now();
+    }
+}
